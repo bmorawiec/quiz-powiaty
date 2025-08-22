a410db43d3a5db8c3f181846f2f95b4cc10d6d0d
Bartosz Morawiec <bartoszmorawiec5@gmail.com>
2025-08-22 12:58:51 +0200

    feat(src/game/common): improve question phrasing

--- src/game/common/questionFormat.ts
+++ src/game/common/questionFormat.ts
@@ -1,71 +1,60 @@
-import { getUnambiguousName, type Guessable, type Unit } from "src/data";
+import { type Unit } from "src/data";
 import type { GameOptions } from "./gameOptions";
-
-
-/** Supported template entries:
- *  %capitals   comma-separated list of capitals
- *  %plates     comma-separated list of car plate prefixes
- *  %prefix     (Jak się nazywa | Jaką stolicę ma | Jakie rejestracje ma | Jaką flagę ma | Jaki herb ma | Znajdź)
- *  %thisType   (to województwo | ten powiat)
- *  %type       (województwo | powiat)
- *  %type|countyType            (województwo | powiat | miasto)
- *  %withCapitalsInCities?      (ze stolicą w mieście | ze stolicami w mieście) or empty string
- *  %unambigName                disambiguated county/voivodeship name */
-const templates: Record<Guessable, string[]> = {
-    name: ["%prefix", "%type|countyType", "%unambigName"],
-    capital: ["%prefix", "%type|countyType", "%withCapitalsInCities?", "%capitals"],
-    plate: ["%prefix", "%type", "z rejestracjami", "%plates"],
-    flag: ["%prefix", "%type", "z tą flagą"],
-    coa: ["%prefix", "%type", "z tym herbem"],
-    map: ["%prefix", "%thisType"],
-};
 
 /** Returns a string containing the a question about the provided administrative unit,
  *  based on the specified game options. */
-export function formatQuestion(unit: Unit, options: GameOptions): string {
-    const template = templates[options.guessFrom];
-    const text = template
-        .map((entry) => {
-            if (entry === "%capitals") {
-                return unit.capitals.join(", ");
-            } else if (entry === "%plates") {
-                return unit.plates.join(", ");
-            } else if (entry === "%prefix") {
-                if (options.guess === "name") {
-                    return "Jak się nazywa";
-                } else if (options.guess === "capital") {
-                    return "Jaką stolicę ma";
-                } else if (options.guess === "plate") {
-                    return "Jakie rejestracje ma";
-                } else if (options.guess === "flag") {
-                    return "Jaką flagę ma";
-                } else if (options.guess === "coa") {
-                    return "Jaki herb ma";
-                } else if (options.guess === "map") {
-                    return "Znajdź";
-                }
-            } else if (entry === "%thisType") {
-                return (options.unitType === "voivodeship") ? "to województwo" : "ten powiat";
-            } else if (entry === "%type") {
-                return (options.unitType === "voivodeship") ? "województwo" : "powiat";
-            } else if (entry === "%type|countyType") {
-                if (unit.type === "county") {
-                    if (unit.countyType === "city") {
-                        return "miasto";
-                    }
-                    return "powiat";
-                }
-                return "województwo";
-            } else if (entry === "%withCapitalsInCities?") {
-                if (unit.countyType === "city") {
-                    return "";
-                }
-                return (unit.capitals.length > 1) ? "ze stolicami w miastach" : "ze stolicą w mieście";
-            } else if (entry === "%unambigName") {
-                return getUnambiguousName(unit);
+export function formatQuestion(unit: Unit, { unitType, guessFrom, guess }: GameOptions) {
+    let str = "";
+    if (guess === "name") {
+        if (guessFrom === "map") {
+            str += (unitType === "voivodeship") ? "Które to województwo?" : "Który to powiat?";
+        } else {
+            str += (unitType === "voivodeship") ? "Które województwo " : "Który powiat ";
+            if (guessFrom === "capital") {
+                str += (unit.capitals.length > 0) ? "ma stolice w miastach " : " ma stolicę w mieście ";
+                str += unit.capitals.join(", ") + "?";
+            } else if (guessFrom === "plate") {
+                str += "ma rejestracje " + unit.plates.join(", ") + "?";
+            } else if (guessFrom === "flag") {
+                str += "ma tą flagę?";
+            } else if (guessFrom === "coa") {
+                str += "ma ten herb?";
             }
-            return entry;
-        })
-        .join(" ");
-    return (options.guess === "map") ? text : text + "?";
+        }
+    } else {
+        if (guess === "capital") {
+            str += "Jaką stolicę ma ";
+        } else if (guess === "plate") {
+            str += "Jakie rejestracje ma ";
+        } else if (guess === "flag") {
+            str += "Jaką flagę ma ";
+        } else if (guess === "coa") {
+            str += "Jaki herb ma ";
+        } else if (guess === "map") {
+            str += "Znajdź ";
+        }
+        if (guessFrom === "map") {
+            str += (unitType === "voivodeship") ? "to województwo" : "ten powiat";
+        } else {
+            str += (unitType === "voivodeship")
+                ? "województwo "
+                : (unit.countyType === "city") ? "miasto " : "powiat ";
+            if (guessFrom === "name") {
+                str += unit.name;
+            } else if (guessFrom === "capital") {
+                str += (unit.capitals.length > 0) ? "ze stolicami w miastach " : "ze stolicą w mieście ";
+                str += unit.capitals.join(", ");
+            } else if (guessFrom === "plate") {
+                str += "z rejestracjami " + unit.plates.join(", ");
+            } else if (guessFrom === "flag") {
+                str += "z tą flagą";
+            } else if (guessFrom === "coa") {
+                str += "z tym herbem";
+            }
+        }
+        if (guess !== "map") {
+            str += "?";
+        }
+    }
+    return str;
 }
