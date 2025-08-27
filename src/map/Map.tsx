f2df465b30740e7cb19b6a58efc435152726ba30
Bartosz Morawiec <bartoszmorawiec5@gmail.com>
2025-08-27 22:06:11 +0200

    fix: use clsx for class names received in props

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { useLayoutEffect, useRef, useState, type ReactNode } from "react";
 import type { Box, Size } from "src/utils/vector";
 import { Viewport } from "./Viewport";
@@ -39,7 +40,7 @@ export function Map({ className, ...viewportProps }: MapProps) {
     return (
         <div
             ref={container}
-            className={"overflow-clip bg-grass-10 dark:bg-gray-90 " + className}
+            className={clsx("overflow-clip bg-grass-10 dark:bg-gray-90", className)}
             onPointerMove={handlePointerMove}
             onPointerLeave={handlePointerLeave}
         >
