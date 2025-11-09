290b637182f4ced6de26687fd4e15e1a0cdf650d
Bartosz Morawiec <bartoszmorawiec5@gmail.com>
2025-11-09 13:01:18 +0100

    fix(src/game): game skeleton pulse animation not playing

--- src/game/GameSkeleton.tsx
+++ src/game/GameSkeleton.tsx
@@ -2,12 +2,12 @@ import { GameLayout, ViewContainer } from "./common";
 
 export function GameSkeleton() {
     return (
-        <GameLayout>
+        <GameLayout className="animate-slow-delayed-fade-in">
             <ViewContainer>
-                <div className="size-full bg-gray-10 animate-slow-delayed-fade-in animate-pulse"/>
+                <div className="size-full bg-gray-10 animate-pulse"/>
             </ViewContainer>
 
-            <div className="w-[400px] rounded-[20px] bg-gray-10 animate-slow-delayed-fade-in animate-pulse"/>
+            <div className="w-[400px] rounded-[20px] bg-gray-10 animate-pulse"/>
         </GameLayout>
     );
 }
