c9c3844d3a521f24a453c0fd598c023145c59878
Bartosz Morawiec <bartoszmorawiec5@gmail.com>
2026-01-16 21:22:21 +0100

    fix(src/game2/ui): fix stepper shifting down when results screen reached

--- src/game2/ui/choice/ChoiceGame.tsx
+++ src/game2/ui/choice/ChoiceGame.tsx
@@ -15,7 +15,7 @@ export function ChoiceGame() {
     return (
         <div className="size-full bg-gray-10 rounded-[20px] px-[20px] py-[50px]
             flex flex-col items-center md:justify-end">
-            <div className={clsx((["flag", "coa"].includes(apiOptions.guess)) ? "md:h-[526px]" : "md:h-[290px]",
+            <div className={clsx((["flag", "coa"].includes(apiOptions.guess)) ? "md:h-[550px]" : "md:h-[320px]",
                 "w-full max-w-[1000px] flex flex-col gap-[30px]")}>
                 <Stepper>
                     {screenIds.map((screenId, index) => 
