313290ba7b69e31c887ec6e528c9f3616660cc83
Bartosz Morawiec <bartoszmorawiec5@gmail.com>
2026-03-06 09:23:31 +0100

    test(src/game2/state/choice): add more tests for actions, naming changes

--- src/game2/state/choice/actions.test.ts
+++ src/game2/state/choice/actions.test.ts
@@ -2,6 +2,7 @@ import type { GameAPICallbacks } from "src/game2/api";
 import type { GameOptions } from "src/gameOptions";
 import type { ZustandHook } from "src/utils/zustand";
 import { describe, expect, it } from "vitest";
+import { createAllActions } from "./actions";
 import { createChoiceGameStore } from "./factory";
 import { ChoiceScreenNotFoundError, type ChoiceGameStore } from "./types";
 
@@ -24,7 +25,7 @@ const emptyCallbacks: GameAPICallbacks = {
 
 /** Simulates a player getting to a question with the specified index.
  *  If the index is omitted, then the final screen will be reached. */
-function reachQuestionWithIndex(store: ZustandHook<ChoiceGameStore>, targetIndex: number = Infinity) {
+function reachQuestionAtIndex(store: ZustandHook<ChoiceGameStore>, targetIndex: number = Infinity) {
     let index = 0;
     while (store.getState().api.state !== "finished" && index < targetIndex) {
         const currentScreenId = store.getState().currentScreenId;
@@ -56,17 +57,27 @@ describe("guess", () => {
 
     it("throws an error when the game is finished", async () => {
         const store = await createChoiceGameStore(someOptions, emptyCallbacks);
-        reachQuestionWithIndex(store);
+        reachQuestionAtIndex(store);
 
         expect(() => store.getState().guess(""))
             .toThrow("This action can only be performed while the game is unpaused.");
     });
 });
 
+describe("nextScreen", () => {
+    it("throws an error when called on the results screen", async () => {
+        const store = await createChoiceGameStore(someOptions, emptyCallbacks);
+        reachQuestionAtIndex(store);
+
+        const { nextScreen } = createAllActions(store.setState, store.getState);
+        expect(() => nextScreen()).toThrow("This action cannot be performed when on the final screen.");
+    });
+});
+
 describe("switchScreens", () => {
     it("throws an error when switching to a screen that hasn't been reached yet", async () => {
         const store = await createChoiceGameStore(someOptions, emptyCallbacks);
-        reachQuestionWithIndex(store, 10);
+        reachQuestionAtIndex(store, 10);
 
         expect(() => store.getState().switchScreens(store.getState().screenIds[5])).not.toThrow();
         expect(() => store.getState().switchScreens(store.getState().screenIds[10])).not.toThrow();
