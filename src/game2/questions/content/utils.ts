5279c2babab3622d4037eb079a4a966c701137c7
Bartosz Morawiec <bartoszmorawiec5@gmail.com>
2026-06-05 20:16:24 +0200

    feat: preload images stored in objects of type TitledImageContent

--- src/game2/questions/content/utils.ts
+++ src/game2/questions/content/utils.ts
@@ -3,7 +3,8 @@ import type { Content } from "./types";
 /** If the provided content contains an image, then this function returns the URL of that image.
  *  Otherwise `null` is returned. */
 export function retrieveImageURL(content: Content): string | null {
-    if (content.type === "image" || content.type === "tallImage" || content.type === "textAndImage") {
+    if (content.type === "image" || content.type === "tallImage" || content.type === "textAndImage"
+            || content.type === "titledImage") {
         return content.url;
     } else if (content.type === "textWithInlineImage") {
         return content.imageUrl;
